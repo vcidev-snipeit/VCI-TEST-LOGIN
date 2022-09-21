d1e008f2182ceb578977540bb8986a9c91da9376
ran020801 <ruben.naanep@gmail.com>
2022-09-21 17:42:54 +0800

    loginform container

--- index.php
+++ index.php
@@ -2,15 +2,19 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
   </head>
 <body>
-  <div id='loginpage' class='loginpage'>
+  <div class='loginpage'>
     <div id='imgcontainer' class='imgcontainer'> 
     </div>
-    <div id='logintab' class='logintab'>
-      <div id='header' class='header'> 
-      </div>
+    <div id='header' class='header'> 
+    </div>
+    <div class='container'>
+      <h1> Log In </h1>
+    </div> 
+    <div class='login'>
     </div>
   </div>
 </body>
