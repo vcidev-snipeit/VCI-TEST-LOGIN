8541b31a59be1acddeb821d4f4b87a80b03ce172
ran020801 <ruben.naanep@gmail.com>
2022-09-22 15:46:07 +0800

    50x50 view

---
+++ 50x50/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title> GWEBSol </title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="stylesheet" href="style.css">
+  </head>
+<body>
+  <div class='loginpage'>
+    <div id='imgcontainer' class='imgcontainer'> 
+    </div>
+    <div id='header' class='header'> 
+    <div class="header-img"></div>
+    </div>
+    </div>
+    <div class='container'>
+      <h1> Log In </h1>
+    </div> 
+    <div class='login'>
+      <h2> Username </h2>
+      <form action="#">
+      <div class="input_box">
+       <input type="text" placeholder="username@vci.com" required>
+       <div class="icon1"><i class="fas fa-user"></i></div>
+     </div>
+     <div class="input_box">
+       <h3> Password </h3>
+       <input type="password" placeholder="Input password" required>
+       <div class="icon2"><i class="fas fa-lock"></i></div>
+       <div class="fpassword">
+       <a href="#" style="text-decoration:none;" target="_blank">Forgot Password?</a>
+      </div>
+      </div>
+     <div class="input_box button">
+       <input type="submit" value="Login">
+    </div>
+    </form>
+    </div>
+  </div>
+</body>
+</html>
+
+
