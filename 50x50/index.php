e43ebffc1dc657f62968db191f1163a277cf9115
ran020801 <ruben.naanep@gmail.com>
2022-09-23 17:39:49 +0800

    sept 23

--- 50x50/index.php
+++ 50x50/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="UTF-8">
-    <title> GWEBSol </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> GWEBSol - Login </title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
@@ -14,7 +14,7 @@
     </div>
     <div id='header' class='header'> 
     <div class="header-img"></div>
-    </div>
+    </div> 
     </div>
     <div class='container'>
       <h1> Log In </h1>
