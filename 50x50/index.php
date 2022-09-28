8c9bf2fc2a705ff67a9abf0553ecceaf4242c0e0
ran020801 <ruben.naanep@gmail.com>
2022-09-28 16:15:01 +0800

    WIP responsive

--- 50x50/index.php
+++ 50x50/index.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">   
     <title> GWEBSol - Login </title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="style.css">
   </head>
+  
 <body>
-  <div class='loginpage'>
-    <div id='imgcontainer' class='imgcontainer'> 
+
+  <div id='imgcontainer' class='imgcontainer'> 
     </div>
     <div id='header' class='header'> 
     <div class="header-img"></div>
-    </div> 
+    </div>
     </div>
     <div class='container'>
       <h1> Log In </h1>
